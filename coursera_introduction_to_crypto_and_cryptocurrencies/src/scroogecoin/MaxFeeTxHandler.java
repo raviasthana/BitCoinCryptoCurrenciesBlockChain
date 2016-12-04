03d956096c80f9b3bedb4a7e721669ea44a9a66a
raviasthana <ravi.asthana@gmail.com>
2016-12-04 18:48:40 +0000

    refactored code

--- coursera_introduction_to_crypto_and_cryptocurrencies/src/scroogecoin/MaxFeeTxHandler.java
+++ coursera_introduction_to_crypto_and_cryptocurrencies/src/scroogecoin/MaxFeeTxHandler.java
@@ -1,11 +1,7 @@
 package scroogecoin;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.PriorityQueue;
 
 public class MaxFeeTxHandler {
@@ -22,47 +18,47 @@ public class MaxFeeTxHandler {
 	 * utxoPool by using the UTXOPool(UTXOPool uPool) constructor.
 	 */
 	public MaxFeeTxHandler(UTXOPool utxoPool) {
-		// IMPLEMENT THIS
 		up = new UTXOPool(utxoPool);
 	}
 
-	/*
-	 * Returns true if (1) all outputs claimed by tx are in the current UTXO
-	 * pool, (2) the signatures on each input of tx are valid, (3) no UTXO is
-	 * claimed multiple times by tx, (4) all of tx's output values are
-	 * non-negative, and (5) the sum of tx's input values is greater than or
-	 * equal to the sum of its output values; and false otherwise.
-	 */
-
+	/** Returns true if
+	   * (1) all outputs claimed by tx are in the current UTXO pool
+	   * (2) the signatures on each input of tx are valid
+	   * (3) no UTXO is claimed multiple times by tx
+	   * (4) all of tx’s output values are non-negative
+	   * (5) the sum of tx’s input values is greater than or equal 
+	   * to the sum of its output values; and false otherwise.
+	   */
 	public boolean isValidTx(Transaction tx) {
 
-		ArrayList<UTXO> seenUTXO = new ArrayList<UTXO>();
+		ArrayList<UTXO> txUTXOList = new ArrayList<UTXO>();
 
-		double inSum = 0;
-		double outSum = 0;
+		double inputSum = 0;
+		double outputSum = 0;
 
 		int index = 0;
 
 		for (Transaction.Input in : tx.getInputs()) {
 
-			UTXO checkUTXO = new UTXO(in.prevTxHash, in.outputIndex);
-			if (seenUTXO.contains(checkUTXO))
-				return false; // 3
-			// no UTXO is claimed multiple times by tx
-
-			seenUTXO.add(checkUTXO);
+			UTXO inUTXO = new UTXO(in.prevTxHash, in.outputIndex);
 
 			// if the transaction pool doesn't contain it already
-			if (!up.contains(checkUTXO))
+			if (!up.contains(inUTXO))
 				return false; // 1
 
-			inSum += up.getTxOutput(checkUTXO).value;
+			inputSum += up.getTxOutput(inUTXO).value;
 
 			// Check Signature
-			if(!Crypto.verifySignature(up.getTxOutput(checkUTXO).address, 
+			if(!Crypto.verifySignature(up.getTxOutput(inUTXO).address, 
 					tx.getRawDataToSign(index), 
 					in.signature))
 				return false; // 2
+
+			// no UTXO is claimed multiple times by tx
+			if (txUTXOList.contains(inUTXO))
+				return false; // 3
+
+			txUTXOList.add(inUTXO);
 
 			index++;
 		}
@@ -70,65 +66,15 @@ public class MaxFeeTxHandler {
 		for (Transaction.Output out : tx.getOutputs()) {
 			if (out.value < 0)
 				return false; // 4
-			outSum += out.value;
+			outputSum += out.value;
 		}
 
-		if (outSum > inSum)
+		if (outputSum > inputSum)
 			return false; // 5
 
 		return true;
 	}
-
-	/*
-	 * Similar to above, but returns either VALID, POT_VALID (if not all inputs
-	 * are in UTXO pool but everything else checks), or INVALID.
-	 */
-	public int classifyTx(Transaction tx) {
-		int result = VALID;
-		ArrayList<UTXO> seenUTXO = new ArrayList<UTXO>();
-
-		double inSum = 0;
-		double outSum = 0;
-
-		int index = 0;
-
-		for (Transaction.Input in : tx.getInputs()) {
-
-			UTXO checkUTXO = new UTXO(in.prevTxHash, in.outputIndex);
-			if (seenUTXO.contains(checkUTXO))
-				return INVALID; // 3
-			// no UTXO is claimed multiple times by tx
-
-			seenUTXO.add(checkUTXO);
-
-			// if the transaction pool doesn't contain it already
-			if (!up.contains(checkUTXO)) {
-				result = POT_VALID;
-			} // 1
-
-			inSum += up.getTxOutput(checkUTXO).value;
-
-			// Check Signature
-			if(!Crypto.verifySignature(up.getTxOutput(checkUTXO).address, 
-					tx.getRawDataToSign(index), 
-					in.signature))
-				return INVALID; // 2
-
-			index++;
-		}
-
-		for (Transaction.Output out : tx.getOutputs()) {
-			if (out.value < 0)
-				return INVALID; // 4
-			outSum += out.value;
-		}
-
-		if (outSum > inSum)
-			return INVALID; // 5
-
-		return result;
-	}
-
+	
 	/*
 	 * classifies transaction AND creates a wrapper.
 	 */
@@ -209,139 +155,10 @@ public class MaxFeeTxHandler {
 	 * UTXO pool as appropriate.
 	 */
 	public Transaction[] handleTxs(Transaction[] possibleTxs) {
-		// return basicHandleTxs(possibleTxs);
-		return greedyHandleTxs(possibleTxs);
-	}
-
-	public Transaction[] basicHandleTxs(Transaction[] possibleTxs) {
-		ArrayList<Transaction> goodTx = new ArrayList<Transaction>();
-
-		boolean isDone = false;
-
-		while (!isDone) {
-			isDone = true;
-
-			for (int i = 0; i < possibleTxs.length; i++) {
-				if (possibleTxs[i] == null)
-					continue;
-				if (isValidTx(possibleTxs[i])) {
-					// Remove old UTXOs from Pool
-					for (Transaction.Input in : possibleTxs[i].getInputs()) {
-						UTXO delUTXO = new UTXO(in.prevTxHash, in.outputIndex);
-						up.removeUTXO(delUTXO);
-					}
-
-					// Add new UTXOs to Pool
-					for (int j = 0; j < possibleTxs[i].getOutputs().size(); j++) {
-						UTXO newUTXO = new UTXO(possibleTxs[i].getHash(), j);
-						up.addUTXO(newUTXO, possibleTxs[i].getOutputs().get(j));
-					}
-
-					goodTx.add(possibleTxs[i]);
-
-					// Set Array Element to Null
-					possibleTxs[i] = null;
-
-					// Not Done Yet!
-					isDone = false;
-				}
-			}
-		}
-
-		Transaction[] tArr = new Transaction[goodTx.size()];
-		tArr = goodTx.toArray(tArr);
-		return tArr;
-	}
-
-	// node for graph
-
-	public class TxWrapper implements Comparable<TxWrapper> {
-		private Transaction tx;
-		private ArrayList<TxWrapper> refs;
-		private double fee;
-		private int validity;
-
-		public TxWrapper(Transaction tx, double fee, int validity) {
-			this.setTx(tx);
-			this.setFee(fee);
-			this.setValidity(validity);
-			this.refs = new ArrayList<TxWrapper>();
-		}
-
-		public Transaction getTx() {
-			return tx;
-		}
-
-		public void setTx(Transaction tx) {
-			this.tx = tx;
-		}
-
-		public ArrayList<TxWrapper> getRefs() {
-			return refs;
-		}
-
-		public int compareTo(TxWrapper tx2) {
-			return Double.compare(fee, tx2.getFee());
-		}
-
-		public double getFee() {
-			return fee;
-		}
-
-		public void setFee(double fee) {
-			this.fee = fee;
-		}
-
-		public int getValidity() {
-			return validity;
-		}
-
-		public void setValidity(int validity) {
-			this.validity = validity;
-		}
-
-		public void addRef(TxWrapper tx2) {
-			refs.add(tx2);
-		}
-	}
-
-	// public double txFee(Transaction tx) {
-	// double inSum=0;
-	// for (Transaction.Input in : tx.getInputs()) {
-	//
-	// inSum += up.getTxOutput(checkUTXO).value;
-	//
-	// // Check Signature
-	// if
-	// (!up.getTxOutput(checkUTXO).address.verifySignature(tx.getRawDataToSign(index),
-	// in.signature))
-	// return INVALID; // 2
-	//
-	// index++;
-	// }
-	//
-	// for (Transaction.Output out : tx.getOutputs()) {
-	// if (out.value < 0) return INVALID; // 4
-	// outSum += out.value;
-	// }
-	//
-	// if (outSum > inSum) return INVALID; // 5
-	// }
-	//
-	// public class TxFeeComparator implements Comparator<Transaction> {
-	//
-	// public int compare(Transaction t1, Transaction t2) {
-	// // TODO Auto-generated method stub
-	// return 0;
-	// }
-	//
-	// }
-
-	public Transaction[] greedyHandleTxs(Transaction[] possibleTxs) {
-
-		// Plan
-		// (1) first create a hash to transaction table for possibleTxs
+		
 		/*
+		 * (1) first create a hash to transaction table for possibleTxs
+		 * 
 		 * (2) for each transaction, if it's invalid, then kill it. If all
 		 * inputs are in UTXOPool, add it to nbrsOfGood. For each input, add
 		 * that transaction to the "refs" list of the referenced address. (3)
@@ -431,108 +248,54 @@ public class MaxFeeTxHandler {
 		return tArr;
 	}
 
-	public class TxHandlerState implements Comparable<TxHandlerState> {
-		public TxHandler handler;
-		public PriorityQueue<TxWrapper> nbrsOfGood;
-		public ArrayList<TxWrapper> potGoodTxs;
-		public ArrayList<Transaction> goodTxs;
-		public double fees;
+	// node for graph
+	public class TxWrapper implements Comparable<TxWrapper> {
+		private Transaction tx;
+		private ArrayList<TxWrapper> refs;
+		private double fee;
+		private int validity;
 
-		public TxHandlerState(TxHandler th) {
-			// to save time, TxHandler should have the UTXOPool scraped
-			// of unnecessary transactions.
-			handler = th;
-			nbrsOfGood = new PriorityQueue<TxWrapper>();
-			potGoodTxs = new ArrayList<TxWrapper>();
-			goodTxs = new ArrayList<Transaction>();
-			fees = 0;
+		public TxWrapper(Transaction tx, double fee, int validity) {
+			this.setTx(tx);
+			this.setFee(fee);
+			this.setValidity(validity);
+			this.refs = new ArrayList<TxWrapper>();
 		}
 
-		// add a copy constructor!
-
-		@Override
-		public int compareTo(TxHandlerState t) {
-			return Double.compare(fees, t.fees);
+		public Transaction getTx() {
+			return tx;
 		}
 
-	}
-
-	public class TxSearch extends HeuristicSearch<TxHandlerState> {
-
-		public TxSearch(TxHandlerState e) {
-			super(e);
+		public void setTx(Transaction tx) {
+			this.tx = tx;
 		}
 
-		@Override
-		boolean test(TxHandlerState e) {
-			return (e.nbrsOfGood.isEmpty());
+		public ArrayList<TxWrapper> getRefs() {
+			return refs;
 		}
 
-		@Override
-		ArrayList<TxHandlerState> children(TxHandlerState e) {
-			// TODO Auto-generated method stub
-
-			return null;
-			// first pass through and add all SAFE transactions
-			// (do this as long as there are more transactions to add)
-			// (define a method isSafe)
-
-			// then take the biggest and create 2 TxHandlerStates: one with it
-			// inside and one without.
-
+		public int compareTo(TxWrapper tx2) {
+			return Double.compare(fee, tx2.getFee());
 		}
 
-	}
-
-	public abstract class HeuristicSearch<E extends Comparable<E>> {
-
-		// protected abstract E state;
-
-		abstract boolean test(E e);
-
-		abstract ArrayList<E> children(E e);
-
-		protected PriorityQueue<E> options;
-
-		public HeuristicSearch(E e) {
-			options = new PriorityQueue<E>();
-			options.add(e);
+		public double getFee() {
+			return fee;
 		}
 
-		// Collections.reverseOrder
-		public HeuristicSearch(E e, Comparator<E> c) {
-			options = new PriorityQueue<E>(11, c);
-			options.add(e);
+		public void setFee(double fee) {
+			this.fee = fee;
 		}
 
-		// trying to minimize the heuristic
-		public E heuristicMinDFS() {
-			while (!options.isEmpty()) {
-				E top = options.poll();
-				if (test(top)) {
-					return top;
-				}
-				options.addAll(children(top));
-			}
-			return null;// solution not found
+		public int getValidity() {
+			return validity;
 		}
 
-		// trying to maximize the heuristic
-		// DO: add a time!
-		E heuristicMaxDFS(int ms) {
-			E best = null;
-			while (!options.isEmpty()) {
-				E top = options.poll();
-				if (test(top)) {
-					if ((best == null) || (top.compareTo(best) == 1)) {
-						best = top;
-					}
-				} else {
-					options.addAll(children(top));
-				}
-			}
-			return best;// solution not found
+		public void setValidity(int validity) {
+			this.validity = validity;
+		}
+
+		public void addRef(TxWrapper tx2) {
+			refs.add(tx2);
 		}
 	}
-
 }
